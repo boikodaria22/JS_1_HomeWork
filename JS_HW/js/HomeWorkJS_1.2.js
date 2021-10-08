e8c5e1abd42e0ace78a0cd9f63b0bb88aa2f9506
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-08 21:42:14 +0300

    Update HomeWorkJS_1.2.js

--- JS_HW/js/HomeWorkJS_1.2.js
+++ JS_HW/js/HomeWorkJS_1.2.js
@@ -2,44 +2,44 @@
 var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 
-for(var i = 0; i < array.length; i++) {  
-  if(isNaN(array[i]) === true) {
-    array[i] = +(array[i])
-  }
-  if(array[i] % 3 === 0) {
-    n++;
-  }
+for (var i = 0; i < array.length; i++) {
+	if (isNaN(array[i]) === true) {
+		array[i] = +(array[i]);
+	}
+	if (array[i] % 3 === 0) {
+		n++;
+	}
 }
-n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`):console.log('В данном массиве нет чисел кратных 3')
+n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
 
 // while
 var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 var i = 0;
-while(i < array.length) {  
-  if(isNaN(array[i]) === true) {
-    array[i] = +(array[i])
-  }
-  if(array[i] % 3 === 0) {
-    n++;
-  }
-  i++
+while (i < array.length) {
+	if (isNaN(array[i]) === true) {
+		array[i] = +(array[i]);
+	}
+	if (array[i] % 3 === 0) {
+		n++;
+	}
+	i++;
 }
-n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`):console.log('В данном массиве нет чисел кратных 3')
+n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
 
 //do...while
 var array = [45, '246', 73, '4', 15, 36, 17, 89, '49', 167, 123];
 var n = 0;
 var i = 0;
 
-do{
-  if(isNaN(array[i]) === true) {
-    array[i] = +(array[i])
-  }
-  if(array[i] % 3 === 0) {
-    n++;
-  }
-  i++
+do {
+	if (isNaN(array[i]) === true) {
+		array[i] = +(array[i]);
+	}
+	if (array[i] % 3 === 0) {
+		n++;
+	}
+	i++;
 }
-while(i < array.length);
-n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`):console.log('В данном массиве нет чисел кратных 3')
+while (i < array.length);
+n > 0 ? console.log(`В данном массиве ${n} чисел кратных 3`) : console.log('В данном массиве нет чисел кратных 3');
