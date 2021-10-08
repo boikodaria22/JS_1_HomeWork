1e8cf0ecc5a07d3e6c48b2efe9f5ab4a291e0f53
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-08 22:37:33 +0300

    Update HomeWorkJS_1.4.js

--- JS_HW/js/HomeWorkJS_1.4.js
+++ JS_HW/js/HomeWorkJS_1.4.js
@@ -13,8 +13,8 @@ wellcome(name, wellcomeString);
 var wellcomeString = 'Nice to see you';
 var name = 'Daria';
 
-var wellcome = function(wellcomeString, name) {
+var wellcome = function(name,wellcomeString) {
 	return (wellcomeString + ', ' + name);
 };
 
-wellcome('Nice to see you', 'Daria');
+wellcome('Daria','Nice to see you');
