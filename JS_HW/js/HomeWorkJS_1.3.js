ddcbc74e49f065d33dfbe083b764803691ef2fba
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-08 21:43:21 +0300

    Update HomeWorkJS_1.3.js

--- JS_HW/js/HomeWorkJS_1.3.js
+++ JS_HW/js/HomeWorkJS_1.3.js
@@ -1,45 +1,45 @@
-var array = [16, true, 'MetraBit', NaN, {name: 'UserName', hobby: 'JS'}, 30, NaN, null, false, 'Vue'];
+var array = [16, true, 'MetraBit', NaN, {
+	name: 'UserName',
+	hobby: 'JS'
+}, 30, NaN, null, false, 'Vue'];
 
 function myFuncForTypes(array) {
-    var num = 0;
-    var str = 0;
-    var bool = 0;
-    var nullType = 0;
-    var obj = 0;
-          for(var i = 0; i<array.length; i++){
+	var num = 0;
+	var str = 0;
+	var bool = 0;
+	var nullType = 0;
+	var obj = 0;
+	for (var i = 0; i < array.length; i++) {
 
-            if(array[i] === null){
-              nullType++;
-            }
-            else{
-              switch(typeof(array[i])) {
-                case 'number':  
-                  num++;
-                  break;
-            
-                case 'string': 
-                  str++;
-                  break;
+		if (array[i] === null) {
+			nullType++;
+		} else {
+			switch (typeof(array[i])) {
+				case 'number':
+					num++;
+					break;
 
-                  case 'boolean': 
-                  bool++;
-                  break;
-            
-                  case 'object':  
-                  obj++;
-                  break;
+				case 'string':
+					str++;
+					break;
 
-                  default:
-                  break;
-            }
-            }
+				case 'boolean':
+					bool++;
+					break;
 
-          }
-          console.log(`Number: ${num}, string: ${str}, boolean: ${bool}, null: ${nullType}, object: ${obj}.`)
-        }
-        myFuncForTypes(array);
+				case 'object':
+					obj++;
+					break;
 
-        
+				default:
+					break;
+			}
+		}
+
+	}
+	console.log(`Number: ${num}, string: ${str}, boolean: ${bool}, null: ${nullType}, object: ${obj}.`);
+}
+myFuncForTypes(array);
 
 
 
