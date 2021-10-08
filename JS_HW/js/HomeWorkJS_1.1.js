2f2f2c3dc4a330ae65ea3b5be275bacdd120a597
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-08 21:38:31 +0300

    Update HomeWorkJS_1.1.js

--- JS_HW/js/HomeWorkJS_1.1.js
+++ JS_HW/js/HomeWorkJS_1.1.js
@@ -1,37 +1,46 @@
 //for
-var array = [16, true, 'MetraBit', NaN, {name: 'UserName', hobby: 'JS'}, 30, NaN, null, false, 'Vue'];
-for(var i = 0; i < array.length; i++) {
-   if(isNaN(array[i]) === true) {
-    array[i] = null;
-    }
+var array = [16, true, 'MetraBit', NaN, {
+	name: 'UserName',
+	hobby: 'JS'
+}, 30, NaN, null, false, 'Vue'];
+for (var i = 0; i < array.length; i++) {
+	if (isNaN(array[i]) === true) {
+		array[i] = null;
+	}
 }
 console.log('Проверка и изменение массива завершена');
 console.log(array);
 
 //while
-var array = [16, true, 'MetraBit', NaN, {name: 'UserName', hobby: 'JS'}, 30, NaN, null, false, 'Vue'];
+var array = [16, true, 'MetraBit', NaN, {
+	name: 'UserName',
+	hobby: 'JS'
+}, 30, NaN, null, false, 'Vue'];
 var i = 0;
-  while( i < array.length) {
-    if(isNaN(array[i]) === true) {
-      array[i] = null;
-    }
-  i++;
-  }
+while (i < array.length) {
+	if (isNaN(array[i]) === true) {
+		array[i] = null;
+	}
+	i++;
+}
 console.log('Проверка и изменение массива завершена');
 console.log(array);
 
 //do...while
-var array = [16, true, 'MetraBit', NaN, {name: 'UserName', hobby: 'JS'}, 30, NaN, null, false, 'Vue'];
+var array = [16, true, 'MetraBit', NaN, {
+	name: 'UserName',
+	hobby: 'JS'
+}, 30, NaN, null, false, 'Vue'];
 var i = 0;
 
 do {
-  {
-    if(isNaN(array[i]) === true) {
-      array[i] = null;
-    }
-  i++;
-  }
+	{
+		if (isNaN(array[i]) === true) {
+			array[i] = null;
+		}
+		i++;
+	}
 }
-  while( i < array.length);
+while (i < array.length);
 console.log('Проверка и изменение массива завершена');
 console.log(array);
